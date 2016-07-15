31d8aad0f1a4722c7c4a3fec2704a957bd0cd4ef
Arnaud Levy <arnaud@semiodesign.com>
2016-07-15 17:40:15 +0200

    wip

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -19,7 +19,7 @@ class ResourcesController < ApplicationController
     @resource = Resource.new resource_params
     respond_to do |format|
       if @resource.save
-        format.html { redirect_to @resource, notice: 'Resource was successfully created.' }
+        format.html { redirect_to edit_resource_path(@resource), notice: 'Resource was successfully created.' }
         format.json { render :show, status: :created, location: @resource }
       else
         format.html { render :new }
